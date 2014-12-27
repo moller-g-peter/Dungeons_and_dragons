e805e82c0e81c1abe86aba39e74005006a23a924
moller-g-peter <=>
2014-12-28 00:32:46 +0100

    .

--- classes/South_Dragon.class.php
+++ classes/South_Dragon.class.php
@@ -16,34 +16,42 @@ class South_Dragon extends Character {
     return $this->allowed_strikes > 0;
   }
 
-  public function set_monster_strike($bool) {
-    $this->monster_strike = $bool;
-  }
+  // public function set_monster_strike($bool) {
+  //   $this->monster_strike = $bool;
+  // }
 
   //an override of the original attack() method
   //also takes in $otherCharacter by reference (&)
-  public function attack(&$otherCharacter) {
+  public function attack(&$otherCharacter, $randomness = 1){
+    // if(!$otherCharacter->isAlive()){
+    //   return $this->name. " tries to attack ".$otherCharacter->name.
+    //   " but ".$otherCharacter->name." is already dead!";
+    // }
+    // elseif (!$this->isAlive()) {
+    //   return $this->name. " tries to attack ".$otherCharacter->name.
+    //   " but ".$this->name." only succeeds in flopping around like a fish!";
+    // }
 
-    //monster no longer has a random monster attack but instead
-    //a monster strike that can be called as an option
-      $monster_super_charge = 1;
-    if ($this->monster_strike && $this->can_strike()) {
-      //supercharge monster attack
-      $monster_super_charge = $this->strength * $this->monster_factor;
-      //and reset monster strike to false so it only happens this time
-      $this->monster_strike = false;
-      //and remove one allowed_strikes
-      $this->allowed_strikes--;
+    $level_diff = $this->level / $otherCharacter->level;
+
+    //if this character has a weapon, add a weapon
+    //multiplier based on the combination between
+    //character strenght and weapon damage
+    $weapon_multiplier = 1;
+    if ($this->weapon) {
+      $weapon_multiplier = $this->weapon->damage;
     }
 
-    //a ternary that determines if any additional text should be added
-    $monster_text = $monster_super_charge > 1 ? //boolean expression to evaluate
-      $this->name." performs a monster attack! " : //if true
-      ""; //if false
+    $damage = round($this->strength * $level_diff * $randomness * $weapon_multiplier);
 
-    //then return the $random_text plus the text that the parent 
-    //class (Character) already returns (see Character class definition)
-    return $monster_text.parent::attack($otherCharacter, $monster_super_charge);
+    // $this -> health += $damage;
+    $otherCharacter -> health -= $damage;
+    if(!$otherCharacter->isAlive()){
+      //increase my level by one when i kill someone
+      $this->level++;
+      return $this->name. " kills ".$otherCharacter->name."!";
+    }
+    return "<br>".$this->name. " breathes fire at ".$otherCharacter->name."!";
   }
 
 }
