29177a3f59fa06627a8f7c4159959c5b23443a28
moller.peter <moller.g.peter@gmail.com>
2014-12-28 16:30:56 +0100

    newly created classes

---
+++ classes/Armour.class.php
@@ -0,0 +1,27 @@
+<?php
+  class Armour extends Base {
+
+    //these properties are protected to make sure
+    //that they are not changed accidentally
+    protected $damage = 3;
+    protected $owner = FALSE;
+
+    public function __construct(&$owner) {
+      //using composition to track a Weapons owner
+      $this->owner = $owner;
+      //and to tell the owner they have a new weapon
+      $this->owner->weapon = $this;
+    }
+
+    //using composition to always return the 
+    //name of my owner
+    public function whos_my_owner() {
+      return "My owner is ".$this->owner->name;
+    }
+
+    //a getter for weapon damage (used in 
+    //a characters attack method)
+    public function get_damage() {
+      return $this->damage;
+    }
+  }
