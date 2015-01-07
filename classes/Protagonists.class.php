86179c811f3b7ce6267d73a3cdbfd2a7288eba23
moller.peter <moller.g.peter@gmail.com>
2015-01-07 23:14:47 +0100

    created a new class for future array

---
+++ classes/Protagonists.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class Protagonists extends character {
+
+	public $protagonists = array(
+	 	array( 'title' => "Item 1",'strength' => 100),
+		array( 'title' => "Item 2",'speed' => 90),
+		array( 'title' => "Item 3",'stamina' => 80),
+	);
+    
+}
