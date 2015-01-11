a93d43a6d888512aed2fadd4ee114e0fe0ea5141
moller-g-peter <=>
2015-01-11 11:50:08 +0100

    oopss..

--- battle_generator.php
+++ battle_generator.php
@@ -1,19 +1,34 @@
 // <?php
 
-// include_once("nodebite-swiss-army-oop.php");
+include_once("nodebite-swiss-army-oop.php");
 
-// $data_base = new DBObjectSaver(array(
-//   "host" => "127.0.0.1",
-//   "dbname" => "wu14oop2",
-//   "username" => "root",
-//   "password" => "mysql",
-//   "prefix" => "Save_data"
-// ));
+$data_base = new DBObjectSaver(array(
+  "host" => "127.0.0.1",
+  "dbname" => "wu14oop2",
+  "username" => "root",
+  "password" => "mysql",
+  "prefix" => "Save_data"
+));
 
-$object_weapons = &$data_base->weapons;
-$object_armors = &$data_base->armors;
-$object_protagonists = &$data_base->protagonists;
-$object_antagonists = &$data_base->antagonists;
+function HP_status($name){
+
+  $health = $name->name." HP: ".$name->health;
+  return $health;
+}
+
+
+// $object_antagonists = array();
+
+//   $object_antagonists[] = New Antagonist("Necro Mancer", array("Health" => 70, "Strength" => 10));
+//   $object_antagonists[] = New Antagonist("South Dragon", array("Health" => 2000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("North Dragon", array("Health" => 3000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Dark Knight", array("Health" => 4000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Owl Eye", array("Health" => 5000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Vampir", array("Health" => 6000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Skeleton", array("Health" => 7000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Manticor", array("Health" => 8000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Ghoul", array("Health" => 9000,"Strength" => 10));
+//   $object_antagonists[] = New Antagonist("Magician", array("Health" => 10000,"Strength" => 10));
 
 // $enemy = $object_necromancer;
 
